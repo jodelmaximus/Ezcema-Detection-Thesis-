5c0b4a18bf800b13954296f68a75f7563db01d2e
Jodel Engot <113647814+jodelmaximus@users.noreply.github.com>
2024-05-19 20:40:57 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,85 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.utils import plot_model
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+# Load the trained model
+model = load_model('eczema_detection_model2conv.keras')
+
+
+
+img_width, img_height = 224, 224
+
+batch_size = 32
+
+
+test_folder = 'test_dataset' 
+
+# Preprocessing
+test_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+#Load Test Data
+test_generator = test_datagen.flow_from_directory(
+    test_folder,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary',  
+    shuffle=False) 
+
+#Class Labels
+predictions = model.predict(test_generator)
+predicted_labels = np.where(predictions > 0.5, 1, 0)  
+true_labels = test_generator.classes
+
+# Evaluation metrics
+accuracy = accuracy_score(true_labels, predicted_labels)
+precision = precision_score(true_labels, predicted_labels)
+recall = recall_score(true_labels, predicted_labels)
+f1 = f1_score(true_labels, predicted_labels)
+cm = confusion_matrix(true_labels, predicted_labels)
+
+# Plot the confusion matrix
+plt.figure(figsize=(8, 6))
+plt.imshow(cm, cmap='Blues')
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+plt.xticks([0, 1], ['Non-eczema', 'Eczema'])
+plt.yticks([0, 1], ['Non-eczema', 'Eczema'])
+plt.colorbar()
+
+thresh = cm.max() / 2
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, format(cm[i, j], 'd'),
+             horizontalalignment="center",
+             color="white" if cm[i, j] > thresh else "black")
+
+
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+print(f'Accuracy: {accuracy * 100:.2f}%')
+print(f'Precision: {precision * 100:.2f}%')
+print(f'Recall: {recall * 100:.2f}%')
+print(f'F1-score: {f1 * 100:.2f}%')
+
+metrics = [accuracy, precision, recall, f1]
+labels = ['Accuracy', 'Precision', 'Recall', 'F1-score']
+
+
+plt.figure(figsize=(8, 6))
+plt.bar(labels, metrics, color=['blue', 'green', 'red', 'orange'])
+plt.title('Model Evaluation Metrics')
+plt.xlabel('Metric')
+plt.ylabel('Value')
+plt.ylim(0, 1)  
+plt.show()
+
