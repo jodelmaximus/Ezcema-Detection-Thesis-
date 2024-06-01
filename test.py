ef97efd2354da098573ed2867b7eefb9860d445c
Jodel Engot <113647814+jodelmaximus@users.noreply.github.com>
2024-06-01 08:58:38 +0800

    update

--- test.py
+++ test.py
@@ -23,7 +23,8 @@ test_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True)
+    horizontal_flip=True,
+    vertical_flip=True)
 
 #Load Test Data
 test_generator = test_datagen.flow_from_directory(
